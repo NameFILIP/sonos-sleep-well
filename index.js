218315b8d3b8dcd723a689f49f55e0de35294f55
Filip Spiridonov <filip.spiridonov@gmail.com>
2017-03-21 22:59:33 -0700

    Enable device auto-discovery

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
 const sonos = require('sonos');
 
-// Note: sonos.search((device) => {}) is an alternative, but it gives a Warning:
-// Possible EventEmitter memory leak detected. 11 timeout listeners added.
-// Use emitter.setMaxListeners() to increase limit
-const host = '10.0.0.31';
-const port = 1400;
-
 const threshold = 50;
-
-
 const sexySongUri = 'https://ia601506.us.archive.org/16/items/SexySaxMan/SexySaxMan.mp3';
 
-const Sonos = new sonos.Sonos(host, port);
-
-console.log(`Watching that Sonos volume is below ${threshold}`);
-global.setInterval(() => {
-  Sonos.getVolume((error1, volume) => {
-    if (error1) {
-      console.error(error1);
-      return;
-    }
-    if (volume > threshold) {
-      console.log(`The volume is above the threshold: ${volume}`);
-      Sonos.setVolume(threshold, (error2) => {
-        if (error2) {
-          console.error(error2);
+sonos.search((device) => {
+  console.log(`Watching that device volume is below ${threshold}`);
+  global.setInterval(
+    () => {
+      device.getVolume((error1, volume) => {
+        if (error1) {
+          console.error(error1);
           return;
         }
-        console.log(`Set volume to ${threshold}`);
-        Sonos.play(sexySongUri, (error3, playing) => {
-          if (error3) {
-            console.error(error3);
-            return;
-          }
-          console.log(`Playing ${playing}`);
-        });
+        if (volume > threshold) {
+          console.log(`The volume is above the threshold: ${volume}`);
+          device.setVolume(threshold, (error2) => {
+            if (error2) {
+              console.error(error2);
+              return;
+            }
+            console.log(`Set volume to ${threshold}`);
+            device.play(sexySongUri, (error3, playing) => {
+              if (error3) {
+                console.error(error3);
+                return;
+              }
+              console.log(`Playing ${playing}`);
+            });
+          });
+        }
       });
-    }
-  });
-}, 10000);
+    },
+    10000,
+  );
+});
